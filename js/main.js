579854697cb1fee6dc44bd97aab1075f83b76dd5
guiyee <guillermodinanno@gmail.com>
2023-04-24 19:01:18 -0300

    finishing notification counter

--- js/main.js
+++ js/main.js
@@ -1,13 +1,38 @@
 const button = document.querySelector(".header-all-read");
 const counter = document.querySelector(".header-counter");
 const notification = document.querySelectorAll(".item")
-const read = document.querySelector(".item-read")
+const read = document.querySelectorAll("#read")
 const message = document.querySelector(".item-message")
 
-//Mark all read Button
-const markRead = (all) => {
-    notification.forEach(x => x.classList.add("item-read"))  
-    
-}
+//Initialize notification counter
+let counterValue = read.length;
+counter.textContent = counterValue;
 
-button.addEventListener("click", markRead)
+//Mark all read Button
+const markRead = (item) => {
+    /* notification.forEach(x => x.classList.add("item-read"))   */
+    for(let i = 0; i < item; i++){
+        notification[i].classList.add("item-read");
+        if(counter.textContent > 0){
+            counter.textContent = counterValue--
+        }
+    }
+}
+button.addEventListener("click", () => {
+    markRead(notification.length);
+
+});
+
+//Mark read on each notification
+notification.forEach(item => {
+    item.addEventListener("click", () => {
+        item.classList.add("item-read");
+
+        if(item.classList.contains("item-read")){
+        counter.textContent = counter.textContent - 1;
+            if(counter.textContent == 0){
+                counter.textContent = counterValue.length
+            }
+        }
+    })
+})
